1f4398e3f346ab277b39263e8d4f7578b965edb6
Donald.E.Lippi <Donald.E.Lippi@noaa.gov>
2019-03-01 17:40:38 +0000

    first commit for this new tool

---
+++ run.ksh
@@ -0,0 +1,38 @@
+#!/bin/ksh
+
+cd /gpfs/hps3/emc/meso/save/Donald.E.Lippi/model_spinup
+mkdir -p figs
+
+fields="ugrdmidlayer tmpmidlayer pressfc spfhmidlayer"
+#fields="ugrdmidlayer"
+#fields="pressfc"
+
+
+for field in $fields; do
+
+cd /gpfs/hps3/emc/meso/save/Donald.E.Lippi/model_spinup
+mkdir $field && cd $field
+cat /dev/null > model_spinup_$field.log
+cp ../templates/model_spinup_template.py model_spinup_$field.py
+sed -i "s/@varname@/$field/g" model_spinup_$field.py
+
+
+cat << EOF > model_spinup_$field.ksh
+#!/bin/ksh
+#BSUB -P FV3GFS-T2O
+#BSUB -J model_spinup_$field
+#BSUB -W 00:25                 # wall-clock time (hrs:mins)
+#BSUB -n 1                     # number of tasks in job
+#BSUB -R "rusage[mem=3072]"    # number of cores
+#BSUB -q "dev"                 # queue
+#BSUB -o model_spinup_$field.log      # output file name in which %J is replaced by the job ID
+
+cd /gpfs/hps3/emc/meso/save/Donald.E.Lippi/model_spinup/$field
+python ./model_spinup_$field.py
+
+EOF
+
+bsub < model_spinup_$field.ksh
+#ksh model_spinup_$field.ksh
+
+done
---
+++ templates/model_spinup_template.py
@@ -0,0 +1,77 @@
+import matplotlib
+matplotlib.use('Agg')   # Need this to generate figs when not running an Xserver (e.g. via PBS/LSF)
+import matplotlib.pyplot as plt
+import os,sys
+import numpy as np
+from netCDF4 import Dataset
+
+datapath="/gpfs/hps2/ptmp/Donald.E.Lippi/fv3gfs_dl2rw/2018091100/NATURE-2018091100-2018091800/gfs.20180911/00/"
+fhr=0
+finc=3
+i=0
+KE=[]
+fcsthour=[]
+fmax=240
+
+varname="@varname@"
+
+while (fhr <= fmax):
+   filename="gfs.t00z.atmf{:03d}.nc4".format(fhr); print(filename)
+   sys.stdout.flush()
+   data_in=os.path.join(datapath,filename)
+   fnd = Dataset(data_in,mode='r')
+
+   if(varname=='ugrdmidlayer' or varname=='tmpmidlayer' or varname=='spfhmidlayer'):
+      KE.append(0.5*(np.abs(fnd.variables[varname][0,:,:,:])).sum(dtype='float')**2)
+
+   if(varname=='pressfc'):
+      KE.append(0.5*(np.abs(fnd.variables[varname][0,:,:])).sum(dtype='float')**2)
+
+   fcsthour.append(fhr)
+   fhr+=finc
+   i+=1
+   fnd.close()
+
+
+
+###################################
+fig = plt.figure(1,figsize=(10,6))#
+fig_title_fontsize=18             #
+xy_label_fontsize=16              #
+tick_label_fontsize=14            #
+legend_fontsize=8.8               #
+dot_size=50                       #
+l_dot_size=7                      #
+linewidth=3                       #
+###################################
+ax = fig.add_subplot(111)
+cticks=np.arange(0,fmax+3,3).tolist()
+
+
+if(varname=='ugrdmidlayer'):
+  color='#000000'
+  label='TKE(ugrd)'
+if(varname=='tmpmidlayer'):
+  color='#ff0044'
+  label='TKE(tmp)'
+if(varname=='pressfc'):
+  color='#55ff00'
+  label='TKE(psfc)'
+if(varname=='spfhmidlayer'):
+  color='#00aaff'
+  label='TKE(spfh)'
+
+ax.plot(fcsthour,KE,color=color,marker='o',markersize=l_dot_size,label=label,linewidth=linewidth,linestyle='-')
+ax.set_xticks(cticks[::4])
+leg=plt.legend(fontsize=legend_fontsize,ncol=4,scatterpoints=1,loc='lower left')
+plt.grid('on')
+title=plt.suptitle("FV3GFS Model Spinup "+label,fontsize=fig_title_fontsize,x=0.5,y=0.95)
+plt.xlabel("Forecast Hour",fontsize=xy_label_fontsize)
+plt.ylabel("Total Kinetic Energy",fontsize=xy_label_fontsize)
+
+leg.get_frame().set_alpha(0.9)
+plt.savefig('../figs/TKE_'+varname+'.png')
+
+
+
+
